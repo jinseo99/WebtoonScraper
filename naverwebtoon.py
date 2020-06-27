faf5034237e981e5f80bd642353170d64eb78885
PotatoEngineer <43867559+jinseo99@users.noreply.github.com>
2020-06-27 02:33:30 -0600

    do again

--- naverwebtoon.py
+++ naverwebtoon.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     
     main_url = r"https://comic.naver.com/webtoon/weekday.nhn"
     sub_url = r"https://comic.naver.com"
-    comic_list = [r'신의 탑', r'윈든브레이커', r'니편내편', r'여신강림', r'랜덤체팅의 그녀', r'제로게임', r'연애혁명', r'외모지상주의', r'유미의 세포들', r'맘마미안']
+    comic_list = [r'신의 탑', r'윈드브레이커', r'니편내편', r'여신강림', r'랜덤채팅의 그녀', r'제로게임', r'연애혁명', r'외모지상주의', r'유미의 세포들', r'맘마미안']
     folder = 'comics/'
     nw = naverwebtoon(main_url, sub_url, comic_list, folder)
     nw.download()
